99465f8ec0d0b13ff551e7f8a0f579c57227156a
Mohammad Samadi <m.samadi1377@gmail.com>
2024-01-20 07:39:50 +0330

    Add project files.

---
+++ src/FluentLicensing.Console/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Security.Cryptography;
+using System.Text;
+
+var aaa = RSA.Create(2048);
+
+var rsa = new RSACryptoServiceProvider(2048);
+var privateKey = rsa.ToXmlString(true);
+var publicKey = rsa.ToXmlString(false);
+
+Console.WriteLine("Private Key: " + Convert.ToBase64String(Encoding.UTF8.GetBytes(privateKey)) + Environment.NewLine);
+Console.WriteLine("Public Key: " + Convert.ToBase64String(Encoding.UTF8.GetBytes(publicKey)) + Environment.NewLine);
+
+var text = "Stringa da Criptare@#[]123123";
+Console.WriteLine("Text to encrypt: " + Environment.NewLine + text + Environment.NewLine);
+var enc = Encrypt(text);
+Console.WriteLine("Encrypted Text: " + Environment.NewLine + enc + Environment.NewLine);
+var dec = Decrypt(enc);
+Console.WriteLine("Decrypted Text: " + Environment.NewLine + dec + Environment.NewLine);
+
+string Encrypt(string data)
+{
+    RSACryptoServiceProvider rsa = new();
+    rsa.FromXmlString(publicKey);
+
+    byte[] dataToEncrypt = Encoding.ASCII.GetBytes(data);
+    byte[] encryptedByteArray = rsa.Encrypt(dataToEncrypt, false).ToArray();
+
+    return Convert.ToBase64String(encryptedByteArray);
+}
+
+string Decrypt(string data)
+{
+    RSACryptoServiceProvider rsa = new();
+    rsa.FromXmlString(privateKey);
+
+    byte[] dataByte = Convert.FromBase64String(data);
+    byte[] decryptedByte = rsa.Decrypt(dataByte, false);
+
+    return Encoding.UTF8.GetString(decryptedByte);
+}
---
+++ src/FluentLicensing/ProtectorService.cs
@@ -0,0 +1,42 @@
+﻿namespace FluentLicensing;
+
+public class ProtectorService
+{
+    public Key PublicKey { get; set; }
+
+    public Key PrivateKey { get; set; }
+}
+
+public class LicenseManager
+{
+    private readonly Key _publicKey;
+    private Dictionary<string, bool> _features;
+    private Dictionary<string, bool> _modules;
+    private Dictionary<string, string> _attributes;
+    private bool _isLicenseValid;
+
+    public string LicenseKey { get; set; }
+
+    public DateOnly ExpiredDate { get; private set; }
+
+    public LicenseManager(Key publicKey, string licenseKey)
+    {
+        _publicKey = publicKey;
+        LicenseKey = licenseKey;
+    }
+
+    public bool IsLicenseValid
+    {
+        get => _isLicenseValid;
+        set => _isLicenseValid = value;
+    }
+
+    public string GetAttribute(string attributeKey)
+        => _attributes.TryGetValue(attributeKey, out var value) ? value : null;
+
+    public bool IsFeatureActive(string feature)
+        => _features.TryGetValue(feature, out var value) && value;
+
+    public bool IsModuleEnable(string moduleName)
+        => _modules.TryGetValue(moduleName, out var value) && value;
+}
